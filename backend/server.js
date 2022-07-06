816c8cc589d0dd1f36eb88cc0d7a5afeb8fc3574
Harsh Patil <harshpatil223@gmail.com>
2022-07-06 18:52:47 +0530

    db connected

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,27 @@
-const express = require('express');
 require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
 
 const app = express();
 
 // middleware
+app.use(express.json()) // built in express middleware
+
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
-app.use("/", (req, res) => {
-    res.json({msg: "welcome"})
-})
+app.use('/api/workouts', workoutRoutes)
 
-app.listen(process.env.PORT, () => {
-    console.log(`listening on port ${process.env.PORT}`)
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`listening on port ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log(err)
 })
