e5767b62e2c8e5c0eff94881a2515be8beaedd18
Harsh Patil <harshpatil223@gmail.com>
2022-07-16 15:57:18 +0530

    rollback

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose')
-const path = require('path')
 const workoutRoutes = require('./routes/workouts')
 
 const app = express();
@@ -16,17 +15,6 @@ app.use((req, res, next) => {
 })
 
 app.use('/api/workouts', workoutRoutes)
-
-__dirname = path.resolve()
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    })
-} else {
-
-}
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
