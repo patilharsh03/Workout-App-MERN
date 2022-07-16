553e498d98fce46a608c92eb136f80be711d9aed
Harsh Patil <harshpatil223@gmail.com>
2022-07-16 14:43:29 +0530

    build for production

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose')
+const path = require('path')
 const workoutRoutes = require('./routes/workouts')
 
 const app = express();
@@ -16,9 +17,20 @@ app.use((req, res, next) => {
 
 app.use('/api/workouts', workoutRoutes)
 
+__dirname = path.resolve()
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
+} else {
+
+}
+
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
-    app.listen(process.env.PORT, () => {
+    app.listen(process.env.PORT || 5000, () => {
         console.log(`listening on port ${process.env.PORT}`)
     })
 })
