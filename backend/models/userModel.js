980253b2fca3e495ef58c652bdc024b9b9c78394
Harsh Patil <harshpatil223@gmail.com>
2022-07-22 15:40:55 +0530

    login and signup auth

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt');
+const validator = require('validator')
 
 const Schema = mongoose.Schema;
 
@@ -15,15 +17,31 @@ const userSchema = new Schema({
 })
 
 // static method
-userSchema.statics.signup = async (email, password) => {
+userSchema.statics.signup = async function (email, password) {
+
+    // validation
+    if (!email || !password) {
+        throw Error('all fields must be filled')
+    }
+    if(!validator.isEmail(email)) {
+        throw Error('email is not valid')
+    }
+    if(!validator.isStrongPassword(password)) {
+        throw Error('Password not strong enough')
+    }
+
     const exists = await this.findOne({ email })
 
     if (exists) {
         throw Error('Email already in use')
     }
 
-    /////////
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
 
+    const user = await this.create({ email, password: hash})
+
+    return user
 }
 
 module.exports = mongoose.model('User', userSchema)
